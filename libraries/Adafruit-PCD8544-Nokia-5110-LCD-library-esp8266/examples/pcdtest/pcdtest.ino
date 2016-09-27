5d316ab940415ca515558caaa70530b6b037cd8a
Дмитрий Олейников <dimao@dimao>
2016-09-27 16:24:46 +0300

    !

--- libraries/Adafruit-PCD8544-Nokia-5110-LCD-library-esp8266/examples/pcdtest/pcdtest.ino
+++ libraries/Adafruit-PCD8544-Nokia-5110-LCD-library-esp8266/examples/pcdtest/pcdtest.ino
@@ -1,10 +1,90 @@
+/*********************************************************************
+This is an example sketch for our Monochrome Nokia 5110 LCD Displays
 
+  Pick one up today in the adafruit shop!
+  ------> http://www.adafruit.com/products/338
+
+These displays use SPI to communicate, 4 or 5 pins are required to
+interface
+
+Adafruit invests time and resources providing this open source code,
+please support Adafruit and open-source hardware by purchasing
+products from Adafruit!
+
+Written by Limor Fried/Ladyada  for Adafruit Industries.
+BSD license, check license.txt for more information
+All text above, and the splash screen must be included in any redistribution
+*********************************************************************/
 
 #include <SPI.h>
 #include <Adafruit_GFX.h>
 #include <Adafruit_PCD8544.h>
 
-Adafruit_PCD8544 display = Adafruit_PCD8544(14, 13, 12, 5, 4);
+// Software SPI (slower updates, more flexible pin options):
+// pin 7 - Serial clock out (SCLK)
+// pin 6 - Serial data out (DIN)
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+Adafruit_PCD8544 display = Adafruit_PCD8544(7, 6, 5, 4, 3);
+
+// Hardware SPI (faster, but must use certain hardware pins):
+// SCK is LCD serial clock (SCLK) - this is pin 13 on Arduino Uno
+// MOSI is LCD DIN - this is pin 11 on an Arduino Uno
+// pin 5 - Data/Command select (D/C)
+// pin 4 - LCD chip select (CS)
+// pin 3 - LCD reset (RST)
+// Adafruit_PCD8544 display = Adafruit_PCD8544(5, 4, 3);
+// Note with hardware SPI MISO and SS pins aren't used but will still be read
+// and written to during SPI transfer.  Be careful sharing these pins!
+
+/******************************************************************
+ESP8266 with PCD8544 display
+
+== Parts ==
+
+* Adafruit Huzzah ESP8266 https://www.adafruit.com/products/2471
+
+* Adafruit PCD8544/5110 display https://www.adafruit.com/product/338
+
+* Adafruit USB to TTL serial cable https://www.adafruit.com/products/954
+
+== Connection ==
+
+USB TTL     Huzzah      Nokia 5110  Description
+            ESP8266     PCD8544
+
+            GND         GND         Ground
+            3V          VCC         3.3V from Huzzah to display
+            14          CLK         Output from ESP SPI clock
+            13          DIN         Output from ESP SPI MOSI to display data input
+            12          D/C         Output from display data/command to ESP
+            #5          CS          Output from ESP to chip select/enable display
+            #4          RST         Output from ESP to reset display
+                        LED         3.3V to turn backlight on
+
+GND (blk)   GND                     Ground
+5V  (red)   V+                      5V power from PC or charger
+TX  (green) RX                      Serial data from IDE to ESP
+RX  (white) TX                      Serial data to ESP from IDE
+******************************************************************/
+
+// ESP8266 Software SPI (slower updates, more flexible pin options):
+// pin 14 - Serial clock out (SCLK)
+// pin 13 - Serial data out (DIN)
+// pin 12 - Data/Command select (D/C)
+// pin 5 - LCD chip select (CS)
+// pin 4 - LCD reset (RST)
+//Adafruit_PCD8544 display = Adafruit_PCD8544(14, 13, 12, 5, 4);
+
+// If using an ESP8266, use this option. Comment out the other options.
+// ESP8266 Hardware SPI (faster, but must use certain hardware pins):
+// SCK is LCD serial clock (SCLK) - this is pin 14 on Huzzah ESP8266
+// MOSI is LCD DIN - this is pin 13 on an Huzzah ESP8266
+// pin 12 - Data/Command select (D/C) on an Huzzah ESP8266
+// pin 5 - LCD chip select (CS)
+// pin 4 - LCD reset (RST)
+//Adafruit_PCD8544 display = Adafruit_PCD8544(12, 5, 4);
 
 #define NUMFLAKES 10
 #define XPOS 0
@@ -34,14 +114,14 @@ static const unsigned char PROGMEM logo16_glcd_bmp[] =
   B00000000, B00110000 };
 
 void setup()   {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   display.begin();
   // init done
 
   // you can change the contrast around to adapt the display
   // for the best viewing!
-  display.setContrast(40);
+  display.setContrast(50);
 
   display.display(); // show splashscreen
   delay(2000);
@@ -187,9 +267,9 @@ void testdrawbitmap(const uint8_t *bitmap, uint8_t w, uint8_t h) {
       icons[f][YPOS] += icons[f][DELTAY];
       // if its gone, reinit
       if (icons[f][YPOS] > display.height()) {
-  icons[f][XPOS] = random(display.width());
-  icons[f][YPOS] = 0;
-  icons[f][DELTAY] = random(5) + 1;
+	icons[f][XPOS] = random(display.width());
+	icons[f][YPOS] = 0;
+	icons[f][DELTAY] = random(5) + 1;
       }
     }
    }
@@ -316,4 +396,3 @@ void testdrawline() {
   }
   delay(250);
 }
-
